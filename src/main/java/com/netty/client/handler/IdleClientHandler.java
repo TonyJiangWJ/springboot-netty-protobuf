d5be188558cbed69155a7970f3b81794a0bcd116
tonyjiangwj <tonyjiangwj@gmail.com>
2018-08-03 09:39:24 +0800

    merge

--- src/main/java/com/netty/client/handler/IdleClientHandler.java
+++ src/main/java/com/netty/client/handler/IdleClientHandler.java
@@ -1,22 +1,10 @@
 package com.netty.client.handler;
 
-<<<<<<< HEAD
-=======
-import java.util.concurrent.TimeUnit;
-
-import org.apache.log4j.Logger;
-
-import com.netty.common.protobuf.Message;
->>>>>>> 5615a426f519adacd6b86ecb2eb0953ffb53fad1
 import com.netty.client.NettyClient;
 import com.netty.common.protobuf.Command.CommandType;
 import com.netty.common.protobuf.Message;
 import com.netty.common.protobuf.Message.MessageBase;
-<<<<<<< HEAD
-=======
-
 import io.netty.bootstrap.Bootstrap;
->>>>>>> 5615a426f519adacd6b86ecb2eb0953ffb53fad1
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.EventLoop;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -24,20 +12,15 @@ import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import org.apache.log4j.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 public class IdleClientHandler extends SimpleChannelInboundHandler<Message> {
-<<<<<<< HEAD
-    private Logger log = Logger.getLogger(this.getClass());
+
+    public Logger log = Logger.getLogger(this.getClass());
 
     private NettyClient nettyClient;
     private int heartbeatCount = 0;
     private final static String CLIENTID = "123456789";
-=======
-	public Logger log = Logger.getLogger(this.getClass());	
-
-	private NettyClient nettyClient;
-	private int heartbeatCount = 0;
-	private final static String CLIENTID = "123456789";
->>>>>>> 5615a426f519adacd6b86ecb2eb0953ffb53fad1
 
     /**
      * @param nettyClient
@@ -46,7 +29,6 @@ public class IdleClientHandler extends SimpleChannelInboundHandler<Message> {
         this.nettyClient = nettyClient;
     }
 
-<<<<<<< HEAD
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if (evt instanceof IdleStateEvent) {
@@ -65,26 +47,6 @@ public class IdleClientHandler extends SimpleChannelInboundHandler<Message> {
             super.userEventTriggered(ctx, evt);
         }
     }
-=======
-	@Override
-	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-		if (evt instanceof IdleStateEvent) {
-			IdleStateEvent event = (IdleStateEvent) evt;
-			String type = "";
-			if (event.state() == IdleState.READER_IDLE) {
-				type = "read idle";
-			} else if (event.state() == IdleState.WRITER_IDLE) {
-				type = "write idle";
-			} else if (event.state() == IdleState.ALL_IDLE) {
-				type = "all idle";
-			}
-			log.debug(ctx.channel().remoteAddress() + "超时类型：" + type);
-			sendPingMsg(ctx);
-		} else {
-			super.userEventTriggered(ctx, evt);
-		}
-	}
->>>>>>> 5615a426f519adacd6b86ecb2eb0953ffb53fad1
 
     /**
      * 发送ping消息
@@ -103,26 +65,16 @@ public class IdleClientHandler extends SimpleChannelInboundHandler<Message> {
         log.info("Client sent ping msg to " + context.channel().remoteAddress() + ", count: " + heartbeatCount);
     }
 
-<<<<<<< HEAD
+
     /**
      * 处理断开重连
      */
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        final EventLoop eventLoop = ctx.channel().eventLoop();
+        eventLoop.schedule(() -> nettyClient.doConnect(new Bootstrap(), eventLoop), 10L, TimeUnit.SECONDS);
         super.channelInactive(ctx);
-        nettyClient.doConnect();
     }
-=======
-	/**
-	 * 处理断开重连
-	 */
-	@Override
-	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		final EventLoop eventLoop = ctx.channel().eventLoop();  
-		eventLoop.schedule(() -> nettyClient.doConnect(new Bootstrap(), eventLoop), 10L, TimeUnit.SECONDS);  
-		super.channelInactive(ctx);  
-	}
->>>>>>> 5615a426f519adacd6b86ecb2eb0953ffb53fad1
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
